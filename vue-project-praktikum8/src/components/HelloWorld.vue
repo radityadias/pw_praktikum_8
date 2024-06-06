45cc8e72508f81422231690d5ff9314d35f33069
radityadias <radityadias24@gmail.com>
2024-06-06 13:50:31 +0700

    Tambah-tambah sedikit

--- vue-project-praktikum8/src/components/HelloWorld.vue
+++ vue-project-praktikum8/src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      Muhammad Raditya Nur Aziz | 22.11.4733
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
